ce9d4b6023c9ba0f601f2cfcd21fbecda81bcdcb
Lab. Desenvolvimento <Lab. Desenvolvimento@172.31.1.183>
2017-03-09 19:45:05 -0300

    Versao1.3 dia 09_03

---
+++ teste/src/teste/Teste.java
@@ -0,0 +1,13 @@
+package teste;
+
+public class Teste
+{
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        System.out.print("Versao1.3");
+
+    }
+
+}
